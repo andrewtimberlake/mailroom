9733d29e9468ee9d1e7b7e97f83ad9f568c79f59
Andrew Timberlake <andrew@andrewtimberlake.com>
2016-09-30 06:29:31 +0200

    Only import environment config if it exists

--- config/config.exs
+++ config/config.exs
@@ -27,4 +27,5 @@ use Mix.Config
 # Configuration from the imported file will override the ones defined
 # here (which is why it is important to import them last).
 #
-import_config "#{Mix.env}.exs"
+environment_config = Path.join([__DIR__, "#{Mix.env}.exs"])
+if File.exists?(environment_config), do: import_config environment_config
