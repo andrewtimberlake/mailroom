3e15202df66c52ce9c9a396184f8a6bd72dfd709
Andrew Timberlake <andrew@andrewtimberlake.com>
2021-06-28 17:39:15 +0200

    Fix crash due to incorrectly formatted date

--- lib/mailroom/imap/envelope.ex
+++ lib/mailroom/imap/envelope.ex
@@ -39,15 +39,39 @@ defmodule Mailroom.IMAP.Envelope do
   def new(list) do
     [date, subject, from, sender, reply_to, to, cc, bcc, in_reply_to, message_id] = list
 
+    # This fixes dates with an incorrect case in the wild
+    date =
+      date
+      |> String.replace(~r/jan/i, "Jan")
+      |> String.replace(~r/feb/i, "Feb")
+      |> String.replace(~r/mar/i, "Mar")
+      |> String.replace(~r/apr/i, "Apr")
+      |> String.replace(~r/may/i, "May")
+      |> String.replace(~r/jun/i, "Jun")
+      |> String.replace(~r/jul/i, "Jul")
+      |> String.replace(~r/aug/i, "Aug")
+      |> String.replace(~r/sep/i, "Sep")
+      |> String.replace(~r/oct/i, "Oct")
+      |> String.replace(~r/nov/i, "Nov")
+      |> String.replace(~r/dec/i, "Dec")
+      |> Mail.Parsers.RFC2822.erl_from_timestamp()
+
+    from = parse_addresses(from)
+    sender = parse_addresses(sender)
+    reply_to = parse_addresses(reply_to)
+    to = parse_addresses(to)
+    cc = parse_addresses(cc)
+    bcc = parse_addresses(bcc)
+
     %__MODULE__{
-      date: Mail.Parsers.RFC2822.erl_from_timestamp(date),
+      date: date,
       subject: subject,
-      from: parse_addresses(from),
-      sender: parse_addresses(sender),
-      reply_to: parse_addresses(reply_to),
-      to: parse_addresses(to),
-      cc: parse_addresses(cc),
-      bcc: parse_addresses(bcc),
+      from: from,
+      sender: sender,
+      reply_to: reply_to,
+      to: to,
+      cc: cc,
+      bcc: bcc,
       in_reply_to: in_reply_to,
       message_id: message_id
     }
