c790062adf2b9065b5a9a631d47355d67ef3b81a
Francesco Zuliani <francesco.zuliani@prima.it>
2021-11-23 11:47:30 +0100

    Add date string to envelope

--- test/mailroom/imap_test.exs
+++ test/mailroom/imap_test.exs
@@ -1192,6 +1192,7 @@ defmodule Mailroom.IMAPTest do
                   bcc: [],
                   cc: [],
                   date: {{2019, 6, 27}, {12, 0, 1}},
+                  date_string: "Thu, 27 Jun 2019 12:00:01 +0200",
                   from: [
                     %Mailroom.IMAP.Envelope.Address{
                       email: "john@example.com",
@@ -1280,6 +1281,7 @@ defmodule Mailroom.IMAPTest do
                   bcc: [],
                   cc: [],
                   date: {{2019, 6, 27}, {12, 0, 1}},
+                  date_string: "Thu, 27 Jun 2019 12:00:01 +0200",
                   from: [
                     %Mailroom.IMAP.Envelope.Address{
                       email: "john@example.com",
